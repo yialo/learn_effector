120c5e3bf5ac06bd2c493c6014b57f0d9a7c96b7
Aleksei Arro <polsck@gmail.com>
2022-04-22 18:45:09 +0300

    feat: add effect call to store-reset example

--- packages/basic/src/learn-core/units/store-reset.ts
+++ packages/basic/src/learn-core/units/store-reset.ts
@@ -1,8 +1,8 @@
-import { createEvent, createStore } from 'effector';
+import { createEvent, createStore, createEffect } from 'effector';
 
 type State = string[];
 
-// Unit creation
+/* Unit creation */
 
 const $items = createStore<State>([]);
 
@@ -10,23 +10,41 @@ const resetEv1 = createEvent();
 const resetEv2 = createEvent();
 const addEv = createEvent<string>()
 
-// Model definition
+const addFx = createEffect<{ value: string }, string, Error>(({ value }) => {
+  return new Promise<string>((resolve) => {
+    setTimeout(() => {
+      console.log('timeout inside addFx!');
+      resolve(value);
+    }, 100);
+  })
+});
+
+/* Model definition */
 
 console.group('Model definition');
+
+// Look: only store watchers called on init, not event/effect ones
 
 $items.watch((items) => {
   console.log('[watch] $items changed:', `[${items.toString()}]`);
 });
 
 $items.on(addEv, (items, newItem) => {
-  console.log('[on] added to $items:', { items: `[${items.toString()}]`, newItem });
+  console.log('[on] added to $items by addEv:', { items: `[${items.toString()}]`, newItem });
 
   return [...items, newItem];
 });
 
+$items.on(addFx.doneData, (items, nextItem) => {
+  console.log('[on] added to $items by addFx.doneData:', { items: `[${items.toString()}]`, nextItem });
+
+  return [...items, nextItem];
+});
+
 $items.reset([resetEv1, resetEv2]);
 
-// Look: event watchers trigger before store watchers, even the former defined after the later
+// Look: event/effect watchers trigger before store watchers, even the former ones defined after the laters
+
 addEv.watch(() => {
   console.log('[watch] addEv called');
 });
@@ -39,11 +57,27 @@ resetEv2.watch(() => {
   console.log('[watch] resetEv2 called');
 });
 
+addFx.watch(() => {
+  console.log('[watch] addFx called');
+});
+
+addFx.doneData.watch(() => {
+  console.log('[watch] addFx.doneData called');
+});
+
+// Look: effect 'done' event calls before 'doneData' one
+
+addFx.done.watch(({ params, result }) => {
+  console.log('[watch] addFx.done called', { params, result });
+});
+
 console.groupEnd();
 
-// Actions
+/* Sync actions */
 
-console.group('Actions');
+console.group('Sync actions');
+
+addFx({ value: 'thing 0' });
 
 addEv('thing 1');
 resetEv1();
@@ -54,10 +88,14 @@ $items.off(resetEv1);
 
 addEv('thing 3');
 
-console.log('-- Look: event is called, but store stays unchanged!');
+console.log('--- Look: event is called, but store stays unchanged');
 
 resetEv1();
 addEv('thing 4');
 resetEv2();
 
 console.groupEnd();
+
+/* Async actions */
+
+console.group('Async actions');
