fafceb4dcd72e8f80bb9b967558b8c154c68fda3
Aleksei Arro <polsck@gmail.com>
2022-04-25 22:52:42 +0300

    feat: add attach-async-fn-source example

---
+++ packages/basic/src/learn-core/operators/attach-async-fn-source.ts
@@ -0,0 +1,20 @@
+import { attach, createStore } from 'effector';
+
+type State = {
+  id: number;
+};
+
+const $product = createStore<State>({ id: 1 });
+
+const derivedFx = attach({
+  source: $product,
+  effect({ id }: State, message: string) {
+    return `Product with ID ${id} requested. ${message}!`;
+  },
+});
+
+derivedFx.watch((message) => {
+  console.log('[watch] derivedFx, message:', message);
+});
+
+derivedFx('Good one!');
