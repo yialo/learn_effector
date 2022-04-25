f830a9ba9a4d317588491c78a2ad2bca3252d8a5
Aleksei Arro <polsck@gmail.com>
2022-04-25 14:07:02 +0300

    feat: add more cases to combine example

--- packages/basic/src/learn-core/operators/combine.ts
+++ packages/basic/src/learn-core/operators/combine.ts
@@ -17,10 +17,19 @@ $numB.watch((state) => {
   console.log('[watch] $numB, state', state);
 });
 
+// NOTE: if last arg is not passed, works like overload with array as first param
 const $combined = combine($numA, $numB, (a, b) => a + b);
+const $combinedObject = combine({ a: $numA, b: $numB }, ({ a, b }) => a + b);
+const $combinedArray = combine([$numA, $numB], ([a, b]) => a + b);
 
 $combined.watch((state) => {
   console.log('[watch] $combined, state', state);
+});
+$combinedObject.watch((state) => {
+  console.log('[watch] $combinedObject, state', state);
+});
+$combinedArray.watch((state) => {
+  console.log('[watch] $combinedArray, state', state);
 });
 
 added(1);
